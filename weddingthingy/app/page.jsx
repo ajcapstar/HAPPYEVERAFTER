1a284470fc08f762c48aba82b36d0f7ef3f20510
Codak <ajcapstar@gmail.com>
2026-04-12 11:25:50 -0500

    adjust spotlight svg height to 99% to prevent overflow

--- weddingthingy/app/page.jsx
+++ weddingthingy/app/page.jsx
@@ -64,24 +64,22 @@ const Page = () => {
         <div className="row">
           <div className="col">
             <div className="card">
-              <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Tenetur quod cum officiis aut odio debitis quisquam. Reiciendis,
-                et magnam laboriosam aspernatur accusamus suscipit? Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Eaque quis sit
-                exercitationem autem accusantium vero recusandae atque facilis a
-                sapiente.
+              <h2> CEREMONY OF LOVE</h2>
+              <p style={{ textAlign: "center", textWrap: "balance" }}>
+                OUR JOURNEY TOGETHER HAS BEEN THE GREATEST ADVENTURE OF OUR
+                LIVES AND WE ARE SO HONOURED TO HAVE YOU WITNESS THE MOMENT WE
+                SAY &quot;I DO&quot;
               </p>
             </div>
           </div>
+          {/* </div>
           <div className="col">
             <ResponsiveImage
               src="/images/image2.jpg"
               alt="desc"
               className="img-container"
             />
-          </div>
+          </div> */}
         </div>
 
         <div className="row">
